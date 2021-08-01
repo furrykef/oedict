f1a2a9ea9ff63a165bd4e8d9c177c7b15844180e
Kef Schecter <furrykef@gmail.com>
2021-08-01 05:44:00 -0500

    Add tool for checking alphabetization of the lexicon
    Also many, many additions to lexicon.txt, for which reason I'm not
    fixing all of the alphabetization yet

--- oedict.py
+++ oedict.py
@@ -15,6 +15,7 @@ def main(argv=None):
     p.add_argument('-r', '--reverse', action='store_true', help="reverse lookup")
     p.add_argument('--dump-index', action='store_true', help="dump debug stuff")
     p.add_argument('--dump-lemmas', action='store_true', help="dump debug stuff")
+    p.add_argument('--abc', action='store_true', help="check lexicon is in alphabetical order")
     p.add_argument('search_terms', nargs='*')
     args = p.parse_args(argv)
     lex = lexicon.Lexicon(args.lexicon)
@@ -22,6 +23,8 @@ def main(argv=None):
         lex.dump_index()
     if args.dump_lemmas:
         lex.dump_lemmas()
+    if args.abc:
+        check_alphabetization(lex)
     for term in args.search_terms:
         lookup(lex, term, args.reverse)
     if args.interactive:
@@ -49,6 +52,18 @@ def lookup(lex, search_str, reverse):
         for entry in entries:
             print(entry.lemma, ':')
             print(textwrap.indent(entry.text, " "*4))
+
+
+def check_alphabetization(lex):
+    alphabet = "aæbcdefghijklmnopqrstþuvwxyz"
+    xlate = str.maketrans("āǣēīōūȳċġ", "aæeiouycg")
+    entries = [x.lemma.replace("-", "").lower().translate(xlate) for x in lex.entries]
+    sorted_entries = sorted(entries, key=lambda s: [alphabet.index(ch) for ch in s])
+    for item1, item2 in zip(entries, sorted_entries):
+        if item1 != item2:
+            print(f"Out of order: {item1} (expected {item2})")
+            return
+    print("All in order")
 
 
 if __name__ == '__main__':
